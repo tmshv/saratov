1780c2921d395ef12edfc45b0a70dbb5cd7d10e3
Roman Timashev <tmshv@yandex-team.ru>
2017-11-18 19:46:50 +0300

    3d tiles styling enchancements

--- Source/map/load3dTiles.js
+++ Source/map/load3dTiles.js
@@ -1,3 +1,11 @@
+function color(hex, alpha) {
+	return `color('${hex}', ${alpha})`;
+}
+
+function attrEqual(attribute, value){
+	return `\${${attribute}} === '${value}'`;
+}
+
 export function load3dTiles(viewer, url, useStyle = false) {
 	const tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
 		url,
@@ -11,18 +19,20 @@ export function load3dTiles(viewer, url, useStyle = false) {
 	}));
 
 	if (useStyle) {
+		const layerName = attrEqual.bind(null, 'layer_name');
+
 		tileset.style = new Cesium.Cesium3DTileStyle({
 			color: {
 				conditions: [
-					["${layer_name} === 'history_type0'", "color('#d12121', 1)"],
-					["${layer_name} === 'history_type2'", "color('#ff6619', 1)"],
-					["${layer_name} === 'history_type3'", "color('#ff73b3', 1)"],
-					["${layer_name} === 'history_type4'", "color('#ffc9e6', 1)"],
-					["${layer_name} === 'transformed_type2'", "color('#004dff', 1)"],
-					["${layer_name} === 'transformed_type3'", "color('#75bfff', 1)"],
-					["${layer_name} === 'transformed_type4'", "color('#bae8ff', 1)"],
+					[layerName('history_type0'), color('#d12121', 1)],
+					[layerName('history_type2'), color('#ff6619', 1)],
+					[layerName('history_type3'), color('#ff73b3', 1)],
+					[layerName('history_type4'), color('#ffc9e6', 1)],
+					[layerName('transformed_type2'), color('#004dff', 1)],
+					[layerName('transformed_type3'), color('#75bfff', 1)],
+					[layerName('transformed_type4'), color('#bae8ff', 1)],
 
-					["true", "color('#FFFFFF', 1.0)"]
+					['true', color('#FFFFFF', 1.0)]
 				]
 			},
 			show: true,
