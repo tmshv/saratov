54812c3f72661ad4939bf31eaf1681233a3430b8
Roman Timashev <tmshv@yandex-team.ru>
2017-11-20 02:13:59 +0300

    interaction updated & bugfixed

--- Source/map/interaction.js
+++ Source/map/interaction.js
@@ -17,23 +17,16 @@ export function initInteraction(viewer) {
 		originalColor: new Cesium.Color()
 	};
 
-	// An entity object which will hold info about the currently selected feature for infobox display
-	var selectedEntity = new Cesium.Entity();
-
-	// Color a feature yellow on hover.
-	viewer.screenSpaceEventHandler.setInputAction(function onMouseMove(movement) {
+	const onMouseMove = movement => {
 		// If a feature was previously highlighted, undo the highlight
-		if (Cesium.defined(highlighted.feature)) {
+		if (highlighted.feature) {
 			highlighted.feature.color = highlighted.originalColor;
 			highlighted.feature = undefined;
 		}
 
 		// Pick a new feature
-		var pickedFeature = viewer.scene.pick(movement.endPosition);
-		if (!Cesium.defined(pickedFeature)) {
-			// nameOverlay.style.display = 'none';
-			return;
-		}
+		const pickedFeature = viewer.scene.pick(movement.endPosition);
+		if (!pickedFeature) return;
 
 		// Highlight the feature if it's not already selected.
 		if (pickedFeature !== selected.feature) {
@@ -41,30 +34,24 @@ export function initInteraction(viewer) {
 			Cesium.Color.clone(pickedFeature.color, highlighted.originalColor);
 			pickedFeature.color = hoverColor;
 		}
-	}, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+	};
 
-	// Color a feature on selection and show metadata in the InfoBox.
-	// 	var clickHandler = viewer.screenSpaceEventHandler.getInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
-	viewer.screenSpaceEventHandler.setInputAction(function onLeftClick(movement) {
+	const onLeftClick = movement => {
 		// If a feature was previously selected, undo the highlight
-		if (Cesium.defined(selected.feature)) {
+		if (selected.feature) {
 			selected.feature.color = selected.originalColor;
 			selected.feature = undefined;
 		}
 
 		// Pick a new feature
-		var pickedFeature = viewer.scene.pick(movement.position);
-		if (!Cesium.defined(pickedFeature)) {
-			// clickHandler(movement);
-			// return;
-		}
-
-		selectFeature(null);
-
-		// Select the feature if it's not already selected
-		if (selected.feature === pickedFeature) {
+		const pickedFeature = viewer.scene.pick(movement.position);
+		if (!pickedFeature) {
+			unselectFeature();
 			return;
 		}
+
+		// Select the feature if it's not already selected
+		if (selected.feature === pickedFeature) return;
 		selected.feature = pickedFeature;
 
 		// Save the selected feature's original color
@@ -78,18 +65,23 @@ export function initInteraction(viewer) {
 		// Highlight newly selected feature
 		pickedFeature.color = selectColor;
 		selectFeature(pickedFeature);
-	}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+	};
+
+	viewer.screenSpaceEventHandler.setInputAction(onMouseMove, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+	viewer.screenSpaceEventHandler.setInputAction(onLeftClick, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+}
+
+function unselectFeature() {
+	selectedFeatureSignal.trigger(null);
 }
 
 function selectFeature(item) {
-	if (!item) {
-		selectedFeatureSignal.trigger(null);
-		return;
-	}
+	if(!item.content) return; // item is not 3d tile
 
-	const feature = item.content.getFeature(0)
+	const feature = item.content.getFeature(0);
 	const attributeNames = feature.getPropertyNames();
-	const attributes = attributeNames.reduce((acc, x) => Object.assign(acc, {
+	const attributes = attributeNames.reduce((acc, x) => ({
+		...acc,
 		[x]: feature.getProperty(x),
 	}), {});
 
