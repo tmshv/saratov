cb24867682d378922981077f20efe3c7ec212a3b
Roman Timashev <tmshv@yandex-team.ru>
2017-12-06 19:06:46 +0300

    zone colors changed

--- Source/models/zones.js
+++ Source/models/zones.js
@@ -3,37 +3,37 @@ export const ZONE_PUBLIC_SPACE = 'zone_public_space';
 export const zones = [
 	{
 		name: 'Зона И1',
-		color: '#ff1966',
+		color: '#ff1940',
 		zoneName: 'zone_h1',
 	},
 	{
 		name: 'Зона И2',
-		color: '#f58e92',
+		color: '#FF7373',
 		zoneName: 'zone_h2',
 	},
 	{
 		name: 'Зона И3',
-		color: '#fbd2d1',
+		color: '#ffa1a1',
 		zoneName: 'zone_h3',
 	},
 	{
 		name: 'Зона ИТ1',
-		color: '#005baa',
+		color: '#004978',
 		zoneName: 'zone_ht1',
 	},
 	{
 		name: 'Зона ИТ2',
-		color: '#73a5d8',
+		color: '#246DAC',
 		zoneName: 'zone_ht2',
 	},
 	{
 		name: 'Зона ИТ3',
-		color: '#b5d2ee',
+		color: '#77ABEE',
 		zoneName: 'zone_ht3',
 	},
 	{
 		name: 'Зона ОП',
-		color: '#00b26b',
+		color: '#7ec395',
 		zoneName: ZONE_PUBLIC_SPACE,
 	},
 	{
