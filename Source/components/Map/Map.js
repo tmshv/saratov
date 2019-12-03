6feb90f16fef0bf85f7aca55621b78d48491e87d
Roman Timashev <roman@tmshv.ru>
2019-12-03 03:34:50 +0300

    Map component style

--- Source/components/Map/Map.js
+++ Source/components/Map/Map.js
@@ -1,71 +1,71 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import className from 'classnames';
 import Cesium from 'cesium/Cesium';
 
 const isFunction = f => typeof f === 'function';
 
 export default class Map extends Component {
-	constructor(props) {
-		super(props);
-		this.onMapRender = this.onMapRender.bind(this);
-	}
+    constructor(props) {
+        super(props);
+        this.onMapRender = this.onMapRender.bind(this);
+    }
 
-	componentWillReceiveProps(newProps) {
-		const {onCanvasRender} = newProps;
-		if (isFunction(onCanvasRender)) {
-			this.unsubscribeCanvasUpdate();
-			this.subscribeCanvasUpdate();
-		} else {
-			this.unsubscribeCanvasUpdate();
-		}
-	}
+    componentWillReceiveProps(newProps) {
+        const { onCanvasRender } = newProps;
+        if (isFunction(onCanvasRender)) {
+            this.unsubscribeCanvasUpdate();
+            this.subscribeCanvasUpdate();
+        } else {
+            this.unsubscribeCanvasUpdate();
+        }
+    }
 
-	subscribeCanvasUpdate() {
-		this.cesium.scene.postRender.addEventListener(this.onMapRender);
-	}
+    subscribeCanvasUpdate() {
+        this.cesium.scene.postRender.addEventListener(this.onMapRender);
+    }
 
-	unsubscribeCanvasUpdate() {
-		this.cesium.scene.postRender.removeEventListener(this.onMapRender);
-	}
+    unsubscribeCanvasUpdate() {
+        this.cesium.scene.postRender.removeEventListener(this.onMapRender);
+    }
 
-	onMapRender() {
-		const {onCanvasRender} = this.props;
-		const canvas = this.cesium.scene.canvas;
-		onCanvasRender(canvas);
-	}
+    onMapRender() {
+        const { onCanvasRender } = this.props;
+        const canvas = this.cesium.scene.canvas;
+        onCanvasRender(canvas);
+    }
 
-	componentDidMount() {
-		const {options, init} = this.props;
-		this.cesium = new Cesium.Viewer(this.element, {
-			...options,
-			creditContainer: this.creditContainer,
-		});
+    componentDidMount() {
+        const { options, init } = this.props;
+        this.cesium = new Cesium.Viewer(this.element, {
+            ...options,
+            creditContainer: this.creditContainer,
+        });
 
-		init(this.cesium);
-	}
+        init(this.cesium);
+    }
 
-	componentWillUnmount() {
-		this.unsubscribeCanvasUpdate();
-		// destroy(this.cesium);
-	}
+    componentWillUnmount() {
+        this.unsubscribeCanvasUpdate();
+        // destroy(this.cesium);
+    }
 
-	render() {
-		const {canInteract} = this.props;
+    render() {
+        const { canInteract } = this.props;
 
-		const cn = className('Map', {
-			'Map--interactive': canInteract,
-		});
+        const cn = className('Map', {
+            'Map--interactive': canInteract,
+        });
 
-		return (
-			<div className={cn}
-				 ref={x => {
-					 this.element = x;
-				 }}
-			>
-				<div ref={x => {
-					this.creditContainer = x;
-				}}/>
-			</div>
-		);
-	}
+        return (
+            <div className={cn}
+                ref={x => {
+                    this.element = x;
+                }}
+            >
+                <div ref={x => {
+                    this.creditContainer = x;
+                }} />
+            </div>
+        );
+    }
 };
